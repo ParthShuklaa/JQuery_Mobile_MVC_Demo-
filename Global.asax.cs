75d1b7bbe24df12b0d4e9539061cc92e5e86396b
Parth Shukla <31998380+ParthShuklaa@users.noreply.github.com>
2021-11-09 10:15:58 +0530

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,46 @@
+﻿using Docker.DotNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.WebPages;
+
+
+namespace Demo_J_Query
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            //WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            BundleMobileConfig.RegisterBundles(BundleTable.Bundles);
+           //AuthConfig.RegisterAuth();
+
+            DisplayModeProvider.Instance.Modes.Insert(1, new DefaultDisplayMode("iPhone")
+            {
+                ContextCondition = (ctx => ctx.GetOverriddenUserAgent().IndexOf("iPhone", StringComparison.OrdinalIgnoreCase) > 0)
+            });
+
+            DisplayModeProvider.Instance.Modes.Insert(1, new DefaultDisplayMode("Android")
+            {
+                ContextCondition = (ctx => ctx.GetOverriddenUserAgent().IndexOf("Android", StringComparison.OrdinalIgnoreCase) > 0)
+            });
+
+            DisplayModeProvider.Instance.Modes.Insert(1, new DefaultDisplayMode("WP")
+            {
+                ContextCondition = (ctx => ctx.GetOverriddenUserAgent().IndexOf("WP", StringComparison.OrdinalIgnoreCase) > 0)
+            });
+
+
+
+        }
+    }
+}
